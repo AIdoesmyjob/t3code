c6b4b49364002d565d8847c8bd4431f2f8d6b736
Dominic Roy <dominic@sdko.org>
2026-09-17 13:20:45 -0400

    fix(web): avoid reopening settled threads when adding projects (#11804)

--- apps/web/src/components/CommandPalette.tsx
+++ apps/web/src/components/CommandPalette.tsx
@@ -1898,7 +1898,7 @@ function OpenCommandPaletteDialog(props: {
           existing.id,
           clientSettings.sidebarThreadSortOrder,
         );
-        if (latestThread) {
+        if (latestThread && latestThread.settledOverride !== "settled") {
           await navigate({
             to: "/$environmentId/$threadId",
             params: buildThreadRouteParams(
